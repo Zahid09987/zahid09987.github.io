a5f131c6ef91eced2d5054d0d1f03e93a9233699
Zahid Rizky Fakhri <zahidrizkyfakhri@gmail.com>
2026-02-02 14:25:29 +0700

    renamed to locale

---
+++ assets/js/i18n.js
@@ -0,0 +1,7 @@
+import { FluentBundle, FluentResource } from "@fluent/bundle";
+import { DOMLocalization } from "@fluent/dom";
+
+// Function to load translations
+async function fetchResource(locale, resourceId) {
+    const response = await fetch(`/locales`)
+}
