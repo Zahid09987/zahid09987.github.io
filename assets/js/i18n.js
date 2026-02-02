9b7f0dcd6251f66e6f09718230d57bed3deee7f4
Zahid Rizky Fakhri <zahidrizkyfakhri@gmail.com>
2026-02-02 14:31:33 +0700

    add fluent.js implementation

--- assets/js/i18n.js
+++ assets/js/i18n.js
@@ -1,7 +1,26 @@
 import { FluentBundle, FluentResource } from "@fluent/bundle";
 import { DOMLocalization } from "@fluent/dom";
 
-// Function to load translations
 async function fetchResource(locale, resourceId) {
-    const response = await fetch(`/locales`)
+  const response = await fetch(`/locale/${locale}/${resourceId}.ftl`);
+  return response.text();
+}
+
+async function initLocalization(locale = "en-US") {
+  const bundle = new FluentBundle(locale);
+  const source = await fetchResource(locale, "main");
+  const resource = new FluentResource(source);
+  bundle.addResource(resource);
+
+  const l10n = new DOMLocalization(
+    [`/locale/${locale}/main.ftl`],
+    (resourceIds) => {
+      return resourceIds.map(() => bundle);
+    },
+  );
+
+  l10n.connectRoot(document.documentElement);
+  l10n.translateRoots();
+
+  return { bundle, l10n };
 }
